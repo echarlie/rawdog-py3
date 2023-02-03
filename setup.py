a8b26de926241370d4de80d9cc120ac19a7c3965
George Rawlinson <george@rawlinson.net.nz>
2023-02-04 04:45:58 +1300

    Fix issues with setup.py (#1)
    * fix version info detection
    
    * fix missing comma
    
    * fix: python version detection
    
    Rather than using sys.version_info to detect Python version,
    upstream Python recommends using python_requires, as found here:
    
    https://packaging.python.org/en/latest/guides/distributing-packages-using-setuptools/#python-requires
    
    * use consistent whitespace (space to tab)

--- setup.py
+++ setup.py
@@ -1,20 +1,16 @@
 #!/usr/bin/env python3
 
 from distutils.core import setup
-import sys
-
-if sys.version_info != (3, ):
-	print("rawdog requires Python 3.")
-	sys.exit(1)
 
 setup(name="rawdog",
 	version="3.0",
 	description="RSS Aggregator Without Delusions Of Grandeur - python3 port",
 	author="echarlie",
 	author_email="echarlie@vtluug.org",
-	url="https://github.com/echarlie/rawdog-py3"
+	url="https://github.com/echarlie/rawdog-py3",
 	scripts=['rawdog'],
 	data_files=[('share/man/man1', ['rawdog.1'])],
+	python_requires='>=3',
 	packages=['rawdoglib'],
 	classifiers=[
 		"Development Status :: 5 - Production/Stable",
